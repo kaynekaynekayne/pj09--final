5561b00024ad5693c045aaeab4fa5600238cff80
kaynekaynekayne <theygoon@gmail.com>
2022-11-06 15:22:08 +0900

    headers styling

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {useUserContext} from '../context/userContext';
 import styled from 'styled-components';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Header = () => {
-    const {user}=useUserContext();
 
     return (
         <HeaderStyle>
             <ul>
-                <li>
-                    <Link to="/">around</Link>
-                </li>
-                <li>
-                    <Link to="/setting">
-                        <AccountCircleIcon />
-                    </Link>
-                </li>
+                <Link to="/">
+                    <li>around</li>
+                </Link>
+                <Link to="/setting">
+                    <li><AccountCircleIcon /></li>
+                </Link>
             </ul>
         </HeaderStyle>
     );
@@ -27,14 +23,7 @@ const HeaderStyle=styled.header`
     width:100%;
     padding:0.8rem;
     background:rgb(54, 81, 254);
-
-    ul{
-        display:flex;
-        justify-content: space-between;
-    }
-    li{
-        color:white;
-    }
+    color:white;
 `;
 
 export default Header;
