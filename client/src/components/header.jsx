9d2a3c12f61db354edc98f9fd01e75b825ccab38
kaynekaynekayne <theygoon@gmail.com>
2022-11-08 18:24:41 +0900

    dealing with warning

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Header = () => {
 
